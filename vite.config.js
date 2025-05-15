2a2b92c86960c01ebc328c9611a365aa02799d11
Patcharapong Samantarn (Bes) <patcharapong@isidsea.th>
2025-05-15 10:18:21 +0700

    Done manifest

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,37 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 
 export default defineConfig({
-  plugins: [tailwindcss(), react()],
+  plugins: [
+    tailwindcss(),
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.svg", "robots.txt", "apple-touch-icon.png"],
+      manifest: {
+        name: "K&B Expense",
+        short_name: "KB",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#F8FAFC",
+        theme_color: "#314158",
+        icons: [
+          {
+            src: "/icon-256.png",
+            sizes: "256x256",
+            type: "image/png",
+          },
+          {
+            src: "/icon-512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
 });
